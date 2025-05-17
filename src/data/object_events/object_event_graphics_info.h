5e86a23f214152db96c8a35e8924d173a1443655
n0dein <donhowmat@gmail.com>
2025-05-17 19:25:43 -0400

    Revert "fixes"
    This reverts commit 0fa51c4684a3271857cdf1d065859afb57125cc1.

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -4674,7 +4674,7 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Follower = {
 
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Treecko = { 
     .tileTag = 0xFFFF,
-    .paletteTag = OBJ_EVENT_PAL_TAG_Treecko,
+    .paletteTag = OBJ_EVENT_PAL_TAG_DYNAMIC,
     .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
     .size = 128,
     .width = 16,
