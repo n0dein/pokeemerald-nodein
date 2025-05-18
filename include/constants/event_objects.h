b679a2983538c5250135dda4b78521ce55bb0567
n0dein <donhowmat@gmail.com>
2025-05-18 15:53:35 -0400

    Update event_objects.h

--- include/constants/event_objects.h
+++ include/constants/event_objects.h
@@ -389,10 +389,10 @@
 #define OBJ_EVENT_PAL_TAG_RS_MAY                  0x1123
 #define OBJ_EVENT_PAL_TAG_DYNAMIC                 0x1124
 #define OBJ_EVENT_PAL_TAG_EXAMPLE                 0x1125
-#define OBJ_EVENT_PAL_TAG_chick2                0x1126
-#define OBJ_EVENT_PAL_TAG_gator3                0x1127
-#define OBJ_EVENT_PAL_TAG_gator1                0x1128
-#define OBJ_EVENT_PAL_TAG_swimmer_toto           0x1129
+#define OBJ_EVENT_PAL_TAG_chick2                  0x1126
+#define OBJ_EVENT_PAL_TAG_gator3                  0x1127
+#define OBJ_EVENT_PAL_TAG_gator1                  0x1128
+#define OBJ_EVENT_PAL_TAG_swimmer_toto            0x1129
 
 #if OW_FOLLOWERS_POKEBALLS
 // Vanilla
