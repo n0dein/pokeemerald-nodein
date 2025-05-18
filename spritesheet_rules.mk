0a8a1a5770446584ef0cb5ec46b79890b1c3192a
n0dein <donhowmat@gmail.com>
2025-05-17 20:43:21 -0400

    I think I finally cracked teh code

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -291,6 +291,9 @@ $(OBJEVENTGFXDIR)/people/nurse.4bpp: %.4bpp: %.png
 $(OBJEVENTGFXDIR)/people/expert_m.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
+$(OBJEVENTGFXDIR)/people/example.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -mwidth 4 -mheight 4
+
 $(OBJEVENTGFXDIR)/people/old_man.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
