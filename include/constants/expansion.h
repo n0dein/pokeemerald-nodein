84405003adb495db9df192c1da07f31e640ecf25
n0dein <donhowmat@gmail.com>
2025-06-14 01:00:38 -0400

    revert

--- include/constants/expansion.h
+++ include/constants/expansion.h
@@ -5,14 +5,18 @@
 #define EXPANSION_VERSION_MAJOR 1
 <<<<<<< HEAD
 #define EXPANSION_VERSION_MINOR 12
+<<<<<<< HEAD
 #define EXPANSION_VERSION_PATCH 1
 =======
 #define EXPANSION_VERSION_MINOR 11
 #define EXPANSION_VERSION_PATCH 4
 >>>>>>> parent of 09ee1d0b2d (Merge branch 'upcoming' into expansion-1.11.4)
+=======
+#define EXPANSION_VERSION_PATCH 0
+>>>>>>> parent of 98c03315a9 (Start of 1.12.1 cycle)
 
 // FALSE if this this version of Expansion is not a tagged commit, i.e.
 // it contains unreleased changes.
-#define EXPANSION_TAGGED_RELEASE FALSE
+#define EXPANSION_TAGGED_RELEASE TRUE
 
 #endif
